04e09be63f81ac55418efd7bca34c25ad8ea5d16
Antony Fourchault <antonyfourchault@gmail.com>
2020-03-28 19:12:44 +0100

    add comment

--- Index_inverse.py
+++ Index_inverse.py
@@ -6,7 +6,7 @@ from Page import Page
 class Index_inverse():
 
     def __init__(self, dirPath):
-        dir = os.listdir(dirPath)
+        dir = os.listdir(dirPath) #liste tous les fichiers d'un répertoire
         pages = list()
         i = 0
         j = 0
@@ -15,27 +15,26 @@ class Index_inverse():
             filePath = dirPath + "/" + fileName
             print(str(j) + "/" + str(len(dir)))
             try:
-                file = open(filePath, 'r', encoding="utf8")
+                file = open(filePath, 'r', encoding="utf8") # lis le fichier avec encodage utf8
                 content = file.read()
-                soup = BeautifulSoup(content, "html.parser")
-                text = soup.get_text()
-                clean_text = ' '.join(text.split())
-                mots = clean_text.split()
-                href = soup.find_all('a', href=True)
+                soup = BeautifulSoup(content, "html.parser") # lib pour parser une page html
+                text = soup.get_text() # recupere uniquement le texte
+                clean_text = ' '.join(text.split()) # supprime les espaces superflu
+                mots = clean_text.split() # le texte devient une liste de mots
+                href = soup.find_all('a', href=True) # recupere les balises de lien
                 liens = list()
                 for a in href:
-                    liens.append(a['href'])
-                pages.append(Page(fileName, mots, liens))
+                    liens.append(a['href']) # recupere les liens contenus dans les balises
+                pages.append(Page(fileName, mots, liens)) #créer un objet page
                 file.close()
             except:
-                i = i + 1
-        # print(str(i) + " fichiers ignorer")
+                i = i + 1 # nb de fichiers ignoré
         self._urlsLoad = len(pages)
         self._pages = pages
         self._indexInverse = dict()
         self.loadIndex()
 
-    def loadIndex(self):
+    def loadIndex(self): # on rempli l'index inversé par rapport au page chargée
         i = 1
         for page in self._pages:
             i=i+1
