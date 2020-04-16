de4fba7a8b56533401067ad7bcf1e281a94cc614
Adrien Cavalieri <adr.cavalieri@gmail.com>
2020-04-16 22:01:39 +0200

    Ajout de la gestion des requêtes POST et récupération de la recherche

--- server.py
+++ server.py
@@ -1,22 +1,47 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import cgi
 
 class Server(BaseHTTPRequestHandler):
 
-    print("Server ready to use...")
-
-    def do_GET(self): #
-        if self.path == '/':
+    def do_GET(self): # Gère les requêtes GET.
+        if self.path.endswith('/'):
             self.path = '/index.html' # Page d'accueil (A).
-        if self.path == '/search':
-            self.path = '/search.html' # Page de recherche (B).
         try:
-            file_to_open = open(self.path[1:]).read() #
+            content = open('html/' + self.path[1:]).read()
             self.send_response(200) # "OK".
         except:
-            file_to_open = "File not found"
+            content = open('html/404.html').read()
             self.send_response(404) # "Page introuvable".
         self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+        self.wfile.write(bytes(content, 'utf-8'))
 
-novasearchServer = HTTPServer(('localhost', 8080), Server) # Serveur tournant sur le port 8080.
-novasearchServer.serve_forever() # Boucle infinie.
+    def do_POST(self): # Gère les requêtes POST.
+        if self.path == '/search':
+            self.path = '/search.html'
+            ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+            pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+            content_len = int(self.headers.get('Content-length'))
+            pdict['CONTENT-LENGTH'] = content_len
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                search = fields.get('reverse_index_search')
+
+                print(search) # Debug : affiche la recherche.
+
+                try:
+                    content = open('html/' + self.path[1:]).read()
+                    self.send_response(200) # "OK"
+                except:
+                    content = open('html/404.html').read()
+                    self.send_response(404) # "Page introuvable".
+                self.end_headers()
+                self.wfile.write(bytes(content, 'utf-8'))
+
+def main():
+    PORT_NUMBER = 8080
+    ADDRESS = 'localhost'
+    novasearchServer = HTTPServer((ADDRESS, PORT_NUMBER), Server) # Serveur tournant sur le port 8080.
+    novasearchServer.serve_forever() # Boucle pour attendre indéfiniment les requêtes HTTP.
+
+if __name__ == '__main__':
+    main()
