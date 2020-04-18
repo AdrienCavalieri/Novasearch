58b7793a52f106ab7c85f2b27261e7bdf178c924
Adrien Cavalieri <adr.cavalieri@gmail.com>
2020-04-18 20:21:13 +0200

    Affichage des liens de pages liées à la recherche sur la page B

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import cgi
+from Index_inverse import Index_inverse
+
+index = Index_inverse("./pages_web")
+index.printIndex()
+
 
 class Server(BaseHTTPRequestHandler):
-
     def do_GET(self): # Gère les requêtes GET.
         if self.path.endswith('/'):
             self.path = '/index.html' # Page d'accueil (A).
@@ -24,12 +28,17 @@ class Server(BaseHTTPRequestHandler):
             pdict['CONTENT-LENGTH'] = content_len
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
-                search = fields.get('reverse_index_search')
+                search = fields.get('reverse_index_search')[0]
 
                 print(search) # Debug : affiche la recherche.
+                results = list(index.recherche(search).keys())
+                s = ""
+                for e in results:
+                    s += "<li class=\"list-group-item\"><a href=\"https://" + e.replace("_","/") + "\">" + e.replace("_","/") + "</a></li>"
+                print(s)
 
                 try:
-                    content = open('html/' + self.path[1:]).read()
+                    content = open('html/' + self.path[1:]).read().replace("{0}", s)
                     self.send_response(200) # "OK"
                 except:
                     content = open('html/404.html').read()
